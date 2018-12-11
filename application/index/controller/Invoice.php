41930af1c0a4b599f9ebbe80dbb47177d25ba382
wujingfeng <1163377596@qq.com>
2018-12-11 18:11:56 +0800

    bug修复

---
+++ application/index/controller/Invoice.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mocha
+ * Date: 2018/12/11
+ * Time: 18:06
+ */
+
+namespace app\index\controller;
+
+
+use think\Request;
+
+class Invoice extends Common
+{
+    public function saveInvoice(){
+        $request = Request::instance();
+
+        
+
+
+    }
+
+}
